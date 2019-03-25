4b0dc63f78e2b24deb97957db4b638f903a6cdc1
AssassinG <hangq_zju@hotmail.com>
2019-03-25 13:05:02 +0800

    commit

--- src/fields/FormFields/TransportContract.js
+++ src/fields/FormFields/TransportContract.js
@@ -3,7 +3,7 @@ import formField from '../BaseFields/FormField.js'
 export default Object.freeze([
     ...formField,
     { key: 'contractNumber', label: '合同号', increate: false},
-    { key: 'onTruckForm', label: '随车清单号', type: 'select', optionsUrl: '/OnTruckForm/query', optionDisplayKey: 'formNumber', increate: false },
+    { key: 'onTruckForm', label: '随车清单', type: 'select', optionsUrl: '/OnTruckForm/query', optionDisplayKey: 'formNumber', increate: false },
     { key: 'project', label: '项目工程', type: 'select', optionsUrl: '/BasicInfo/Project/query', optionDisplayKey: 'name', increate: false},
     { key: 'outStorageForm', label: '出库单号', type: 'select', optionsUrl: '/OutStorageForm/query', optionDisplayKey: 'outStorageNumber', increate: false},
     { key: 'truck', label: '运输车辆', type: 'select', optionsUrl: '/BasicInfo/Truck/query', optionDisplayKey: 'carNumber', increate: false },
